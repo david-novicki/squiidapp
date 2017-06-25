a85e6fbf71df77bcde77d06d9fd1ea5390da2f89
Gage <gagelangdon@gmail.com>
2017-06-25 10:59:03 -0700

    add logo to splash

--- ios/squiidapp/AppDelegate.m
+++ ios/squiidapp/AppDelegate.m
@@ -24,7 +24,7 @@
                                                       moduleName:@"squiidapp"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:3f green:169f blue:244f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
